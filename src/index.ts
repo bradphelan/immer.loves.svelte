852f078f839b91b1ba61224f0523c8f0954e0667
brad <brad@moduleworks.com>
2020-12-04 20:35:48 +0100

    bump

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export {subStore} from './lib/subStore';
 export {undoStore, UndoRedoStore} from './lib/undoStore';
+export {arrayStore} from './lib/arrayStore'
 export {transactionStore, TransactionStore} from './lib/transactionStore';
 export { sortStore} from './lib/sortStore'
